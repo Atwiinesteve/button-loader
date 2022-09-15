971f27e26a7500dd104b59333fc5864567ccbc4d
Kiiza Atwiine Stephen <stephenkiiza123@gmail.com>
2022-09-15 15:30:37 +0300

    Loader goes away as soon as data is successfully fetched.

--- app.js
+++ app.js
@@ -1,12 +1,56 @@
 ( function() {
 
+ const users = document.querySelector('.users')
  const btn = document.querySelector('#search-button');
  const loader = document.querySelector('#search-button div');
 
- btn.addEventListener('click', runLoader);
+ btn.addEventListener('click', loadData);
 
- function runLoader() {
-  loader.classList.toggle('loading')
- }
+ function loadData() {
+  loader.classList.add('loading');
+
+  let usersData = fetch('https://jsonplaceholder.typicode.com/users')
+   usersData
+    .then((response) => response.json())
+    .then((data) => {
+
+     loader.classList.remove('loading');
+     btn.remove();
+     data.forEach(user => {
+      users.innerHTML += `
+   
+          <div class="user--container">
+          <div class="name">
+           <p>${user.name}</p>
+          </div>
+          <div class="username">
+           <p>${user.username}</p>
+          </div>
+          <div class="email">
+           <p>${user.email}</p>
+          </div>
+          <div class="address">
+           <p>${user.address.street}</p>
+          </div>
+          <div class="phone">
+           <p>${user.phone}</p>
+          </div>
+          <div class="website">
+           <p>${user.website}</p>
+          </div>
+          <div class="company">
+           <p>${user.company.name}</p>
+          </div>
+         </div>
+      
+      `;
+
+    loader.classList.remove('loading')
+   });
+   
+  })
+  .catch((error) => { console.log(error.message) })
+
+ };
 
 } ());
